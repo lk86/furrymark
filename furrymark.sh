ed2ff198cfa31255a186b0d8ff3ec5f162602c6b
Linus Hemingway Kromwell <git@lhk.me>
2016-09-30 15:50:14 -0400

    MORE COLORS

--- furrymark.sh
+++ furrymark.sh
@@ -9,20 +9,24 @@ COLS=`tput cols`
 LINES=`tput lines`
 size=$(($COLS * $LINES))
 #screen=$(eval printf "%.0s$o" {1..$size})
+RANDOM=$$
 
 fg() {
     echo "\033[38;5;$(($RANDOM % 256))m"
 }
     
 os() {
-    eval printf "%.0s$o" {1..$(($RANDOM % 19))}
+    eval printf "%.0s$o" {1..$(($RANDOM % 19 + 10))}
 }
-        
-while [[ ${#screen} -lt $size ]]; do
-    col="$(fg)"
-    ((size+=${#col}))
-    screen="${screen}${col}$(os)"
-done
+
+scree() {
+    RANDOM=$RANDOM
+    while [[ ${#screen} -lt $size ]]; do
+        col="$(fg)"
+        ((size+=${#col}))
+        screen="${screen}${col}$(os)"
+    done
+}
 
 trap "check" SIGUSR1
 trap "cleanup" SIGTERM SIGINT
@@ -43,6 +47,8 @@ cleanup() {
     exit
 }
 
+scree
+
 watch -tpe -n 1 "kill -USR1 $$" &
 pid=$!
 
@@ -50,5 +56,6 @@ while :
 do
     echo -ne $screen
     let frames++
+    scree
     #screen=${screen//o҉o҉o҉o҉o҉o҉o҉o҉o҉o҉o҉/$(fg)o҉o҉o҉o҉$(fg)o҉o҉o҉o҉o҉o҉$(fg)o҉o҉o҉o҉o҉o҉}
 done
